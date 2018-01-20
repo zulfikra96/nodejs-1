128026e86e37f5bcfd57ef7076466b76c29e5974
cross <cross.network.software@gmail.com>
2018-01-20 09:33:58 +0700

    add new

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const http  = require('http')
-const url   = require('url')
-const routes = require('routes')()
+const http      = require('http')
+const url       = require('url')
+const routes    = require('routes')()
+const view      = require('swig')
 
 
-
+// route
 routes.addRoute("/",function(req,res){
-    res.writeHead(200,{'Content-Type' : 'text/plain'})
-    res.end("hello")
+    let title = ['zulfikra','lahmudin','rifaizal']
+    html  = view.compileFile('./template/home/index.html')({
+        title:title,
+    })
+    
+    res.writeHead(200,{'Content-Type' : 'text/html'})
+    res.end(html)
 })
+
+routes.addRoute("/detail/:name",function(req,res){
+    res.writeHead(200,{'Content-Type' : 'text/plain'})
+    res.end("hello " + this.params.name)
+})
+
 
 
 // server
